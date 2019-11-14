a3c35c3ca20f7369e6fac9342d636aa1e56b6d69
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-11-14 18:33:08 +0000

    done

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ const MONGODB_URI = 'mongodb://localhost/recipeApp';
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true,
+    useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to Mongo!');
+    console.log('Connected to Mongo!')
   })
   .catch(err => {
     console.error('Error connecting to mongo', err);
-  });
+  })
+  .finally(() => {
+    mongoose.connection.close();
+  })
+
+  // Recipe.deleteOne(
+  //   { title: "Carrot Cake"}) 
+  //   .then(() => console.log("gone"))
+
+  
